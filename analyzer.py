1398cde36e4a545683ae83f390ed04c9e09510f7
ying.tan <ying.tan@tcl.com>
2015-03-09 10:20:03 +0800

    initial

---
+++ analyzer.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+#analyze power consumption
+#ying.tan@tcl.com
+
+import sys
+
+RESULT_FILE = 'consumption_arrange_result.txt'
+WAKELOCK_FILTER = 'PowerManagerService: releaseWakeLockInternal:'
+FILTER_INDEX = 82 #line.find(WAKELOCK_FILTER) + len(WAKELOCK_FILTER) + 1
+
+class Record:
+    def __init__(self):
+        self.id = ''
+        self.key = ''
+        self.total_time = None
+
+def validate_path(string):
+    return string
+
+def change_time_to_int(string):
+    return int(string [:-3])
+
+def display_consumption(sorted_list, key_dict):
+    string = ''
+    for item in sorted_list:
+        string += key_dict[item[0]] + ': ' + str(item[1]) + '\n'
+
+    f = open(RESULT_FILE, 'w')
+    result = str(string)
+    f.write(result)
+    f.close()
+
+    print 'Consumption arranged successed!\nResult output in: ' + RESULT_FILE
+
+def count_consumption(record_list):
+    count_dict = {}
+    key_dict = {}
+    for record in record_list:
+        if not count_dict.has_key(record.id):
+            count_dict[record.id] = change_time_to_int(record.total_time)
+            key_dict[record.id] = record.key
+        else:
+            count_dict[record.id] += change_time_to_int(record.total_time)
+
+    sorted_list = sorted(count_dict.iteritems(), key=lambda d:d[1], reverse = True )  
+    #display the result
+    display_consumption(sorted_list, key_dict)
+
+def main():
+    argv_len = len(sys.argv)
+    record_list = []
+
+    if argv_len < 2:
+        print('should input log path!')
+    else:
+        #should validate log path
+        log_path = validate_path(sys.argv[1])
+        fIn = open(log_path, 'rb')
+        for  line in  fIn.readlines():
+            filter_index = line.find(WAKELOCK_FILTER)
+            if filter_index > -1:
+                #change line into object
+                line = line[FILTER_INDEX:]
+                record = Record()
+                record.id = line[5:15] #lock=1120168048
+                line = line[16:]
+                temp_index = line.find('],') + 3
+                record.key = line[:temp_index-2]
+                item_list = line[temp_index:].split(' ')
+                record.total_time = (item_list[1][11:]) #len('total_time=')
+                # print record.total_time
+                #push the object into dict by key
+                record_list.append(record)
+
+        #count the consumption by key
+        sorted_list = count_consumption(record_list)
+
+
+if __name__ == '__main__':
+    main()
